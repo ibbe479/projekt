ea0daf3c1071d517ce47d5c072f54b63292c29bc
ibbe479 <ibrahim.halawani04@gmail.com>
2026-06-03 16:01:23 +0200

    nu är betting funktionen helt klar
    behöver bara css
    
    Co-Authored-By: abbe040 <205142621+abbe040@users.noreply.github.com>

--- moneyxchange/src/Betting.jsx
+++ moneyxchange/src/Betting.jsx
@@ -2,20 +2,40 @@ import { useState, useEffect } from "react";
 
 function Betting() {
   const [loading, setLoading] = useState(true);
+
   const [summaPengar, setsummaPengar] = useState(() => {
     const sparadSumma = localStorage.getItem("summa");
     if (sparadSumma) {
-      return JSON.parse(sparadSumma);
+      return Number(sparadSumma);
     } else {
-      return 1000;
+      localStorage.setItem("summa", 1000);
+      return Number(1000);
     }
   });
   const [sparadValuta, setSparadValuta] = useState(() => {
     const sparadval = localStorage.getItem("sparadValutaLocal");
     if (sparadval) {
-      return JSON.parse(sparadval);
+      return Number(sparadval);
     } else {
-      return 0;
+      return Number(0);
+    }
+  });
+
+  const [köpt, setKöpt] = useState(() => {
+    const tradeStatusBuy = localStorage.getItem("tradeStatusbuy");
+    if (tradeStatusBuy == "true") return true;
+    else {
+      localStorage.setItem("tradeStatusbuy", false);
+      return false;
+    }
+  });
+
+  const [sell, setSell] = useState(() => {
+    const tradeStatusSell = localStorage.getItem("tradeStatussell");
+    if (tradeStatusSell == "true") return true;
+    else {
+      localStorage.setItem("tradeStatussell", false);
+      return false;
     }
   });
 
@@ -36,10 +56,7 @@ function Betting() {
       .then((response) => response.json())
       .then((result) => {
         setSparadValuta(result.quotes.USDSEK);
-        localStorage.setItem(
-          "sparadValutaLocal",
-          JSON.stringify(result.quotes.USDSEK),
-        );
+        localStorage.setItem("sparadValutaLocal", result.quotes.USDSEK);
         setLoading(false);
       })
       .catch((error) => console.log("error", error));
@@ -54,8 +71,76 @@ function Betting() {
   };
 
   function buy() {
-    summaPengar;
-    sparadValuta;
+    if (!köpt && !sell) {
+      return (
+        <div>
+          <button className="btn btn-success" onClick={startBuy}>
+            Buy
+          </button>
+          <button className="btn btn-danger" onClick={startSell}>
+            Sell
+          </button>
+        </div>
+      );
+    } else if (köpt) {
+      return (
+        <div>
+          <button className="btn btn-info" onClick={avslutaBuy}>
+            Close Trade
+          </button>
+        </div>
+      );
+    } else if (sell) {
+      return (
+        <div>
+          <button className="btn btn-info" onClick={avslutaSell}>
+            Close Trade
+          </button>
+        </div>
+      );
+    }
+  }
+
+  function startBuy() {
+    setKöpt(true);
+    localStorage.setItem("tradeStatusbuy", true);
+
+    localStorage.setItem("inköpsKurs", sparadValuta);
+    localStorage.setItem("summaPåkonto", summaPengar);
+  }
+
+  function startSell() {
+    setSell(true);
+    localStorage.setItem("tradeStatussell", true);
+
+    localStorage.setItem("inköpsKurs", sparadValuta);
+    localStorage.setItem("summaPåkonto", summaPengar);
+  }
+
+  function avslutaBuy() {
+    setKöpt(false);
+    localStorage.setItem("tradeStatusbuy", "false");
+
+    const inköpsKurs = Number(localStorage.getItem("inköpsKurs"));
+
+    const resutatAvKurserna = (sparadValuta - inköpsKurs) * 1000;
+
+    const resutatetAvSumman = Number(summaPengar) + resutatAvKurserna;
+    localStorage.setItem("summa", resutatetAvSumman.toFixed(2));
+    setsummaPengar(resutatetAvSumman);
+  }
+
+  function avslutaSell() {
+    setSell(false);
+    localStorage.setItem("tradeStatussell", "false");
+
+    const inköpsKurs = Number(localStorage.getItem("inköpsKurs"));
+
+    const resutatAvKurserna = (inköpsKurs - sparadValuta) * 1000;
+
+    const resutatetAvSumman = Number(summaPengar) + resutatAvKurserna;
+    localStorage.setItem("summa", resutatetAvSumman.toFixed(2));
+    setsummaPengar(resutatetAvSumman);
   }
 
   return (
@@ -73,11 +158,11 @@ function Betting() {
         ditt resultat
       </p>
 
-      <p>{summaPengar}</p>
+      <p>{summaPengar.toFixed(2)}</p>
       <p>USDxSEK ligger på {kollaLoading()} just nu</p>
-      <button className="btn btn-success">Buy</button>
-      <button className="btn btn-danger">Sell</button>
-      {/*<button className="btn btn-info">Close Trade</button>*/}
+      {buy()}
+
+      {/**/}
     </div>
   );
 }
